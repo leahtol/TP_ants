6788d781ec4e8f23fdb7e751d4f780758095b1a4
Leah Toledano <leah.toledano@etu.minesparis.psl.eu>
2026-01-06 16:04:32 +0100

    q4

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import flet as ft
 import random
 import math
 
+class Student:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.matiere={}
+        pass
+
+
 def main(page: ft.Page):
     page.title="Algorithme de colonies de fourmis"
     page.theme_mode=ft.ThemeMode.LIGHT
@@ -30,11 +38,13 @@ def main(page: ft.Page):
         
         nodes = []
         for _ in range(num_nodes):
-            x = random.uniform(50, 550)  # Marges de 50px
+            x = random.uniform(50, 550)
             y = random.uniform(50, 450)
             nodes.append((x, y))
         
+        distances = calculer_distances()
         print(f"{len(nodes)} nœuds générés")
+        print(f"Distance entre nœud 0 et 1 : {distances[0][1]:.2f}")
         dessiner_graphe()
     
     def dessiner_graphe():
@@ -60,7 +70,23 @@ def main(page: ft.Page):
             height=500
         )
         page.update()
-        
+    
+    def calculer_distances():
+        n=int(noeuds.value)
+        M = [] 
+        for i in range(n):
+            row = []
+            for j in range(n):
+                if i == j:
+                    row.append(0)
+                else:
+                    # Distance euclidienne
+                    dx = nodes[i][0] - nodes[j][0]
+                    dy = nodes[i][1] - nodes[j][1]
+                    distance = math.sqrt(dx * dx + dy * dy)
+                    row.append(distance)
+            M.append(row)
+        return M
     # Bouton pour générer le graphe
     btn_generer = ft.Button(
         "Générer le Graphe",
